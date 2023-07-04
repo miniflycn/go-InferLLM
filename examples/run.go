559049ab54b47a13ba62160960943a30f8f4f7dc
Daniel Yang <miniflycn@justany.net>
2023-07-04 09:48:22 +0800

    update

--- examples/run.go
+++ examples/run.go
@@ -13,11 +13,14 @@ func main() {
 		fmt.Println("Loading the model failed:", err.Error())
 		os.Exit(1)
 	}
+	// 问一个问题，并获取回答
 	p := l.Ask("怎么提高睡眠?")
 	print(p)
 	for {
+		// 如果回答没有结束继续获取
 		p := l.GetContinue()
 		print(p)
+		// 回答结束了，退出循环
 		if InferLLM.IsEnd(p) {
 			break
 		}
